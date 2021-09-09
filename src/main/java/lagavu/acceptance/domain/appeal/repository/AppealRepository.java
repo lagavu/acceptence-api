178a77446e66a7933ffa4c3922107a4af1e767c4
vladimir <usdbuy@mail.ru>
2021-09-09 12:11:01 +0700

    added appeal repository

--- src/main/java/lagavu/acceptance/domain/appeal/repository/AppealRepository.java
+++ src/main/java/lagavu/acceptance/domain/appeal/repository/AppealRepository.java
@@ -3,5 +3,5 @@ package lagavu.acceptance.domain.appeal.repository;
 import lagavu.acceptance.domain.appeal.entity.Appeal;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface AppealRepository  extends JpaRepository<Appeal, Long> {
+public interface AppealRepository extends JpaRepository<Appeal, Long> {
 }
