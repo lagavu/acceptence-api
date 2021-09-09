edd8f8770f30389032294bd13b3c54a80f9d6dc1
vladimir <usdbuy@mail.ru>
2021-09-09 11:58:24 +0700

    adding date format for data formatter

--- src/main/java/lagavu/acceptance/util/date_formatter/DateFormatter.java
+++ src/main/java/lagavu/acceptance/util/date_formatter/DateFormatter.java
@@ -5,6 +5,8 @@ import java.util.Date;
 
 public class DateFormatter {
 
+    public static final String DATE_FORMAT_dd_MM_yyyy = "dd-MM-yyyy";
+
     public static String getCurrentDate(String format) {
         SimpleDateFormat formatter = new SimpleDateFormat(format);
         Date date = new Date();
--- src/main/java/lagavu/acceptance/util/document_creator/exception/DocumentCreatorException.java
+++ src/main/java/lagavu/acceptance/util/document_creator/exception/DocumentCreatorException.java
@@ -1,5 +1,9 @@
 package lagavu.acceptance.util.document_creator.exception;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(value = HttpStatus.BAD_REQUEST)
 public class DocumentCreatorException extends RuntimeException {
 
     public DocumentCreatorException(String errorMessage) {
