011885f9b137143738839efe8a4edce918339296
vladimir <usdbuy@mail.ru>
2021-09-09 12:04:12 +0700

    added customer controller

---
+++ src/main/java/lagavu/acceptance/controller/CustomerController.java
@@ -0,0 +1,28 @@
+package lagavu.acceptance.controller;
+
+import lagavu.acceptance.domain.customer.dto.CustomerDto;
+import lagavu.acceptance.domain.customer.entity.Customer;
+import lagavu.acceptance.domain.customer.mapper.CustomerMapper;
+import lagavu.acceptance.domain.customer.repository.CustomerRepository;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class CustomerController {
+
+    private final CustomerRepository customerRepository;
+
+    public CustomerController(CustomerRepository customerRepository) {
+        this.customerRepository = customerRepository;
+    }
+
+    @GetMapping("/customer/{id}")
+    public ResponseEntity<CustomerDto> get(@PathVariable Long id) {
+        Customer customer = customerRepository.getById(id);
+
+        return new ResponseEntity<CustomerDto>(CustomerMapper.INSTANCE.toDto(customer), HttpStatus.OK);
+    }
+}
